ac20dc8907a2ff78831cd2c7f880207b97946b69
demxp <demxp@ya.ru>
2018-11-13 23:42:22 +0500

    Тестовая страница в root маршруте

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -26,7 +26,7 @@
 
             .flex-center {
                 align-items: center;
-                display: flex;
+                display: block;
                 justify-content: center;
             }
 
@@ -42,10 +42,12 @@
 
             .content {
                 text-align: center;
+                width: 50%;
+                margin: 20px auto;
             }
 
             .title {
-                font-size: 84px;
+                font-size: 24px;
             }
 
             .links > a {
@@ -61,38 +63,117 @@
             .m-b-md {
                 margin-bottom: 30px;
             }
+
+            .reqtoken{
+                text-align: left;
+                border: 1px solid gray;
+                padding: 10px;                
+            }
+
+            .content input{
+                width:100%;
+            }
         </style>
     </head>
     <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
-                        @endif
-                    @endauth
-                </div>
-            @endif
-
+        <div class="flex-center position-ref full-height" id="vueapp">
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Тестирование
                 </div>
 
-                <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                <div>
+                    <div class="reqtoken">
+                        <h4>Запрос токена авторизации</h4>
+                        <label>Логин</label>
+                        <br />
+                        <input type="text" v-model="user.login" />
+                        <br />
+                        <label>Пароль</label>
+                        <br />
+                        <input type="text" v-model="user.password" />
+                        <br />
+                        <button type="button" style="display: block; margin: 5px auto;" @click="gettoken">Запрос</button>
+                        <div class="servresp">
+                            Ответ сервера: <span v-text="loginresponse"></span>                            
+                        </div>
+                    </div>
+                    <div class="reqtoken">
+                        <h4>Запрос погоды</h4>
+                        <label>Токен</label>
+                        <br />
+                        <input type="text" v-model="user.token" />
+                        <br />
+                        <button type="button" style="display: block; margin: 5px auto;" @click="getweather">Запрос</button>
+                        <div class="servresp">
+                            Ответ сервера:<br /><span v-html="weather"></span>
+                            <hr />
+                            Ответ сервера - JSON:<br /><span v-text="weatherresponse"></span>                            
+                        </div>
+                    </div>                    
                 </div>
             </div>
         </div>
     </body>
+    <script src="https://cdn.jsdelivr.net/npm/vue/dist/vue.js"></script>
+    <script>
+        const app = new Vue({
+          el: '#vueapp',
+          data(){
+            return{
+              user: {
+                login: null,
+                password: null,
+                token: null
+              },
+              loginresponse: "",
+              weatherresponse: ""
+            }
+          },
+          mounted(){
+            
+          },
+          methods:{
+            ajaxfun(url, method, body=null, token=null, callback){
+                let headers = {};
+                if(!!token){
+                    headers["token"] = token;
+                }
+                headers["Content-type"] = "application/json; charset=UTF-8";
+                fetch(url, {
+                  method: method,
+                  headers: headers,
+                  body: (body !== null) ? JSON.stringify(body) : null
+                }).then(response => {
+                    return response.json();
+                }).then(req => {
+                    return callback(req);
+                }).catch(e => {
+                    console.log(e);
+                });         
+            },
+            gettoken(){
+                this.ajaxfun("/auth", "POST", this.user, null, (req) => {
+                    this.loginresponse = req;
+                })
+            },
+            getweather(){
+                this.ajaxfun("/weather", "GET", null, this.user.token, (req) => {
+                    this.weatherresponse = req;
+                })
+            },            
+          },
+          computed:{
+            weather: function(){
+                if(this.weatherresponse == ""){ return "";}
+                if(!! this.weatherresponse.status){
+                    return "Ошибка: " + this.weatherresponse.status;
+                }
+                return this.weatherresponse.reduce((acc, current)=>{
+                    return acc += current.town + ": " + current.current_temp + '&deg;C<br />';
+                }, '')
+            }
+          }
+        });        
+    </script>
 </html>
